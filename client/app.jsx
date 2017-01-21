075ba7c8fec5c424c84187c5204578bbc449ef0f
rebekahlow-jy <rebekahlow.jy@u.nus.edu>
2017-01-21 20:18:03 +0800

    Added more styling.

--- client/app.jsx
+++ client/app.jsx
@@ -39,19 +39,36 @@ var App = React.createClass({
 		return (
 			<div className="app-container">
 				<div className="area-name-block">
-					<div className="area-name">Prototype #1</div>
+					<div className="area-name">Koufu</div>
 				</div>
-				<div className="seats-block">
-					<div className="empty-seats">
-						<p className="empty-seats-number">8</p>
-						<p className="empty-seats-label">EMPTY</p>
+				<div className="seats-block-container">
+					<div className="seats-block empty-seats-color">
+						<div className="seats-number">{this.state.numEmpty}</div>
+						<div className="seats-label">EMPTY</div>
 					</div>
-					<div className="occupied-seats">
-						<p className="occupied-seats-number">0</p>
-						<p className="occupied-seats-label">OCCUPIED</p>
+					<span className="space-filler" />
+					<div className="seats-block occupied-seats-color">
+						<div className="seats-number">{this.state.numFull}</div>
+						<div className="seats-label">OCCUPIED</div>
 					</div>
 				</div>
-				<div className="visualised-seats"></div>
+				<div className="visualised-seats-container">
+					<div className="visualised-seats-block">
+						<div className="seat top left"></div>
+						<div className="seat top right"></div>
+						<div className="table"></div>
+						<div className="seat bottom left"></div>
+						<div className="seat bottom right"></div>
+					</div>
+					<span className="space-filler" />
+					<div className="visualised-seats-block">
+						<div className="seat top left"></div>
+						<div className="seat top right"></div>
+						<div className="table"></div>
+						<div className="seat bottom left"></div>
+						<div className="seat bottom right"></div>
+					</div>
+				</div>
 			</div>
 		);
 	}
